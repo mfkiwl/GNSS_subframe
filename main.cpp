2e19a6e70210f4a27233a9c31115b58cd3374089
Aleksander Veksler <a.veksler@fugro.com>
2023-07-02 01:46:06 +0200

    improvements

--- main.cpp
+++ main.cpp
@@ -1,87 +1,101 @@
 #include <iostream>
 #include <cassert>
+#include <map>
 #include <boost/dynamic_bitset.hpp>
 
+// GNSS_data namespace contains all the classes related to GNSS data handling
 namespace GNSS_data {
-    struct field {
+    // A struct to hold field information
+    struct Field {
         size_t start, length;
     };
 
-    class frame {
-        boost::dynamic_bitset<> data;
+    typedef const std::map<std::string, Field> FieldMap;
 
-        class field_accessor {
+    // Frame class is the basic class to hold and manipulate a frame of GNSS data
+    class Frame {
+        boost::dynamic_bitset<> data;
+        FieldMap& fields;
+
+        // A helper class to provide access to individual fields within a frame
+        class FieldAccessor {
             boost::dynamic_bitset<>& parent;
-            field f;
+            Field field;
         public:
-            explicit field_accessor(boost::dynamic_bitset<>& parent,
-                                    field f):
-                    parent(parent), f(f) {}
+            explicit FieldAccessor(boost::dynamic_bitset<>& parent, Field field) : parent(parent), field(field) {}
+
+            // This operator returns a part of the parent bitset representing the field
             operator boost::dynamic_bitset<>() const {
-                boost::dynamic_bitset<> result(f.length);
-                for (size_t i = 0; i < f.length; ++i) {
-                    result[i] = parent[f.start + i];
+                boost::dynamic_bitset<> result(field.length);
+                for (size_t i = 0; i < field.length; ++i) {
+                    result[i] = parent[field.start + i];
                 }
                 return result;
             }
-            /**
-             * @throw std::overflow_error
-             */
+
+            // This operator allows casting to an unsigned long
             operator unsigned long() const {
                 boost::dynamic_bitset<> result = *this;
                 return result.to_ulong();
             }
 
-            field_accessor& operator=(const boost::dynamic_bitset<>& in)  {
-                assert (in.size() == f.length);
-                for (size_t i = 0; i < f.length; ++i) {
-                    parent[f.start + i] = in[i];
+            // Overloaded assignment operator to handle assignment of a bitset to a field
+            FieldAccessor& operator=(const boost::dynamic_bitset<>& inputBitset) {
+                assert(inputBitset.size() == field.length);
+                for (size_t i = 0; i < field.length; ++i) {
+                    parent[field.start + i] = inputBitset[i];
                 }
                 return *this;
             }
 
-            field_accessor& operator=(const unsigned long in){
-                boost::dynamic_bitset<> b(f.length, in);
-                return (*this) = b;
+            // Overloaded assignment operator to handle assignment of an unsigned long to a field
+            FieldAccessor& operator=(const unsigned long inputNumber) {
+                boost::dynamic_bitset<> bitset(field.length, inputNumber);
+                return (*this) = bitset;
             }
         };
+
     public:
-        explicit frame(const int size) : data(size) {}
-        std::map<const std::string, field> fields;
-        field_accessor operator[](const std::string& fieldName) {
-            assert (this->fields.count(fieldName));
-            return field_accessor(data, fields[fieldName]);
+        explicit Frame(const uint size, const FieldMap& fields) : data{size}, fields{fields} {}
+
+        // Overloaded [] operator to access individual fields by their names
+        FieldAccessor operator[](const std::string& fieldName) {
+            assert(this->fields.count(fieldName));
+            return FieldAccessor(data, fields.at(fieldName));
         }
     };
 
-    class Glonass_line : public frame {
+    // GlonassLine is a specific type of Frame used in GLONASS systems
+    class GlonassLine : public Frame {
         // 170 chips, 2 chips per bit, 8 hamming bits, 1 idle bit.
         static const int len{170/2-8-1};
-    public:
-        Glonass_line() : frame(len) {}
+    protected:
+        explicit GlonassLine(FieldMap& fields) : Frame(len, fields) {}
     };
 
-    class Glonass_line2: public Glonass_line {
+    const std::map<std::string, Field> GlonassLine2Fields{
+            {"m",  {0, 4}},
+            {"Bn", {4, 3}},
+            {"P2", {7, 1}},
+            {"tb", {8,7}},
+            {"UNUSED1", {15,5}},
+            {"yn_dot", {20, 24}},
+            {"yn_dot_dot", {44, 5}},
+            {"yn", {49, 27}}
+    };
+
+    // GlonassLine2 is a specific type of GlonassLine with a specific set of fields
+    class GlonassLine2: public GlonassLine {
     public:
-        explicit Glonass_line2() {
-         this->fields.insert({
-                    {"m",  {0, 4}},
-                    {"Bn", {4, 3}},
-                    {"P2", {7, 1}},
-                    {"tb", {8,7}},
-                    {"UNUSED1", {15,5}},
-                    {"yn_dot", {20, 24}},
-                    {"yn_dot_dot", {44, 5}},
-                    {"yn", {49, 27}}
-            });
-        }
+        explicit GlonassLine2(): GlonassLine(GlonassLine2Fields) {}
     };
 }
 
 int main() {
-    using GNSS_data::Glonass_line2;
-    Glonass_line2 g;
+    using GNSS_data::GlonassLine2;
+    GlonassLine2 g;
     g["Bn"] = 3;
     unsigned long Bn = g["Bn"];
+    g["does not exist"] = 3;
     std::cout << Bn << std::endl;
 }
