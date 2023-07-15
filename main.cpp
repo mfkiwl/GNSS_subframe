5091b352d693210ffb89f68489b246ef9739a144
Aleksander Veksler <a.veksler@fugro.com>
2023-07-15 22:20:01 +0200

    Template-based

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cassert>
-#include <map>
 #include <boost/dynamic_bitset.hpp>
 #include <bitset>
 
@@ -59,57 +58,132 @@ namespace GNSS_data {
                 {"n",           {69, 5}},
                 {"M",           {74, 2}}
         };
-        template<size_t N_fields>
-        consteval bool name_in_list(const Field (&in)[N_fields], const char* name) {
-            bool found{false};
-            for (auto field: in) {
-                if (strcmp(field.name, name) == 0 ){
-                    found = true;
-                }
-            }
-            return found;
-        }
 
-        constexpr int strcmp(const char *a, const char *b) {
-            while(*a && (*a == *b)) {
+        static constexpr int strcmp(const char *a, const char *b) {
+            while (*a && (*a == *b)) {
                 ++a;
                 ++b;
             }
             return static_cast<unsigned char>(*a) - static_cast<unsigned char>(*b);
         }
-        template<size_t N_fields, const Field (&fields)[N_fields]>
-        struct field_map {
-             const Field &operator[](const char *name)  const {
-                static_assert(name_in_list(fields, "m"));
-                //const bool found{false};
-                for (int i = 0; i < N_fields; i++) {
-                    if (strcmp(fields[i].name, name) == 0) {
-                        return fields[i];
-                    }
+
+        template<size_t N_fields>
+        static constexpr const Field& get_field(const Field (&in)[N_fields], const char *name) {
+            for (auto& field: in) {
+                if (strcmp(field.name, name) == 0) {
+                    return field;
                 }
-                throw std::out_of_range(name);
             }
-        };
-        typedef field_map<sizeof(GlonassLine1)/sizeof(GlonassLine1[0]), GlonassLine1> GlonassLine1Map;
+            throw std::out_of_range(name);
+        }
 
-        template<size_t N_bits, size_t N_fields, const Field (&fields)[N_fields]>
-        struct Subframe {
+        template<size_t N_bits, size_t N_fields, const Field(& fields)[N_fields]>
+        class Subframe {
+        private:
+            // A helper class to provide access to individual fields within a frame
+            class FieldAccessor {
+                std::bitset<N_bits>& parent;
+            public:
+                const Field& field;
+                explicit constexpr FieldAccessor(std::bitset<N_bits>& parent, const Field& field) : parent(parent),
+                                                                                          field(field) {}
+
+                // This operator returns a part of the parent bitset representing the field
+                operator boost::dynamic_bitset<>() const {
+                    boost::dynamic_bitset<> result(field.loc.length);
+                    for (size_t i = 0; i < field.loc.length; ++i) {
+                        result[i] = parent[field.loc.start + i];
+                    }
+                    return result;
+                }
+
+                // This operator allows casting to an unsigned long
+                operator unsigned long() const {
+                    return static_cast<boost::dynamic_bitset<>>(*this).to_ulong();
+                }
+
+                // Overloaded assignment operator to handle assignment of a bitset to a field
+                FieldAccessor &operator=(const boost::dynamic_bitset<>& inputBitset) {
+                    assert(inputBitset.size() == field.loc.length);
+                    for (size_t i = 0; i < field.loc.length; ++i) {
+                        parent[field.loc.start + i] = inputBitset[i];
+                    }
+                    return *this;
+                }
+
+                // Overloaded assignment operator to handle assignment of an unsigned long to a field
+                FieldAccessor &operator=(const unsigned long inputNumber) {
+                    boost::dynamic_bitset<> bitset(field.loc.length, inputNumber);
+                    return (*this) = bitset;
+                }
+            };
+        public:
+
+            FieldAccessor operator[](const char* fieldName) {
+                return FieldAccessor(data, get_field(fields,fieldName));
+            }
+
+            // The name has to be externally linked to be used as a template parameter.
+            template<const char* name>
+            FieldAccessor get() {
+                return FieldAccessor(data, get_field(fields, name));
+
+            }
+
+            template<uint field_index>
+            FieldAccessor get() {
+                static_assert(field_index<N_fields);
+                return FieldAccessor(data, fields[field_index]);
+            }
             std::bitset<N_bits> data;
-            bool get() {
-                return data[0];
+            struct iterator {
+                size_t i;
+                std::bitset<N_bits>& parent;
+                FieldAccessor operator*() {
+                    return FieldAccessor(parent, fields[i]);
+                }
+                iterator& operator++() {
+                    i++;
+                    return *this;
+                }
+
+                bool operator!=(const iterator& other) const {
+                    return this->i != other.i;
+                }
+            };
+
+        public:
+            iterator begin() {
+                return iterator{0, data};
+            }
+
+            iterator end() {
+                return iterator{N_fields, data};
             }
         };
-        typedef Subframe<76, sizeof(GlonassLine1)/sizeof(GlonassLine1[0]), GlonassLine1> Glonass_line_1;
     }
-    using impl::Glonass_line_1;
-    typedef impl::field_map<sizeof(impl::GlonassLine1)/sizeof(impl::GlonassLine1[0]), impl::GlonassLine1> Glonass_line_1_map;
 
+    typedef impl::Subframe<76, sizeof(impl::GlonassLine1)/sizeof(impl::GlonassLine1[0]), impl::GlonassLine1> GlonassLine1_subframe;
+    typedef impl::Subframe<76, sizeof(impl::GlonassLine2)/sizeof(impl::GlonassLine2[0]), impl::GlonassLine2> GlonassLine2_subframe;
 }
 int main() {
+    GNSS_data::GlonassLine1_subframe gs;
+    gs["tk"] = 3;
 
+    auto v= gs.get<6>();
+    v = 23;
+    std::cout << gs["m"];
+    for (auto i: gs) {
+        std::cout << i.field.name << ": " << i << "; ";
+    }
+
+    auto p = std::tuple{
+            GNSS_data::GlonassLine1_subframe(),
+            GNSS_data::GlonassLine2_subframe(),
+    };
     //GNSS_data::Glonass_line_1 b;
-    GNSS_data::Glonass_line_1_map m;
-    std::cout << m["m"].loc.length << std::endl;
+
+
 //    b.data[0] = 1;
 //    std::cout << b.get() << std::endl;
 
